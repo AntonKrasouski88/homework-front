83f15de5a3627945cbb3ef5584c4dc7def880853
Anton Krasovski <antohkad21@gmail.com>
2024-06-15 13:54:38 +0300

    fix: input and display of the task in h1

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -16,8 +16,7 @@ export const HW3 = () => {
   ]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let newText = event.currentTarget.value
-    setCurrentText(newText)
+    setCurrentText(event.currentTarget.value)
   };
 
   const handleSave = () => {
@@ -28,7 +27,7 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
         ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
